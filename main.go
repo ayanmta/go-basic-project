6442e4134d8924fa32eb5da30bcad4917171759c
Ayan Mehta <48169693+ayanmta@users.noreply.github.com>
2025-04-04 10:35:31 +0530

    Added case-sensitive option to Contains function

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func Contains[T comparable](slice []T, element T) bool {
+func Contains[T comparable](slice []T, element T, caseSensitive bool) bool {
+	if str, ok := any(element).(string); ok && !caseSensitive {
+		for _, v := range slice {
+			if vStr, ok := any(v).(string); ok {
+				if strings.EqualFold(vStr, str) {
+					return true
+				}
+			}
+		}
+		return false
+	}
 
 	for _, v := range slice {
 		if v == element {
@@ -15,9 +26,7 @@ func Contains[T comparable](slice []T, element T) bool {
 }
 
 func main() {
-	fmt.Println(Contains([]int{1, 2, 3, 4}, 3))
-	fmt.Println(Contains([]string{"ayan", "mehta"}, "mehtaa"))
-
-	fmt.Println(Contains([]string{"ayan", "mehta"}, "mehtaa"))
-
+	fmt.Println(Contains([]int{1, 2, 3, 4}, 3, true))
+	fmt.Println(Contains([]string{"ayan", "mehta"}, "MEHTA", false))
+	fmt.Println(Contains([]string{"ayan", "mehta"}, "mehtaa", true))
 }
